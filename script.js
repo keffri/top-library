9b3f98ccdf9ff1b3b60a6f175a106146dbd061d8
keffri <keffri.w.neal@gmail.com>
2021-02-10 17:22:36 -0800

    basic functions

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+  }
+}
